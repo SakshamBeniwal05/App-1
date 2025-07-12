a9061aa55934f490d49b88973ac98b2b13e2ebe0
Saksham <sakshambeniwal123@gmail.com>
2025-07-12 23:52:40 +0530

    ....

--- src/components/containers/login.jsx
+++ src/components/containers/login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { login as reduxLogin } from '../../storage/slices/status'
 import authService from "../../services/auth";
 import { useDispatch } from "react-redux";
@@ -6,8 +6,10 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
     const dispatch = useDispatch();
     const navigate  = useNavigate();
+    const [error, setError] = useState("")
+
     async function Triggred_Login(data) {
-        
+
         // const session = await authService.login(data)
         //     .then(async () => {
         //         const userData = await authService.currentUser();
@@ -22,6 +24,7 @@ const Login = () => {
         // Another version of code use to deliver the outcome it can works but i found that it is not optimal so now using {try() catch()} for oprimal outcome
 
         async function Triggred_Login(data) {
+            setError("")
             try {
                 const session = await authService.login(data)
                 if (session) {
@@ -38,6 +41,7 @@ const Login = () => {
                     console.log(`component : containers : login.jsx : getting session : ${error}`);
                 }
             } catch (error) {
+                setError(error.message)
                 console.log(`component : containers : login.jsx : ${error}`);
             }
         }
