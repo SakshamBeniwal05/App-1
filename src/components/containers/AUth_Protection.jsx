70c559549c08c01bfb1241d46aaa3962d5a39879
Saksham <sakshambeniwal123@gmail.com>
2025-07-15 00:06:01 +0530

    laoder animation!

--- src/components/containers/AUth_Protection.jsx
+++ src/components/containers/AUth_Protection.jsx
@@ -23,14 +23,45 @@ const Protection = ({ children, authentication = true }) => {
 
 
 
-    return Loader ?
-        (<div className="flex justify-center items-center h-screen">
-            <div className="relative w-[65px] aspect-square">
-                <div className="absolute rounded-full box-border shadow-inner border-[3px] border-white animate-l5"></div>
-                <div className="absolute box-border shadow-inner border-[3px] border-white animate-l5 rounded-none -delay-[1.25s]"></div>
-            </div>
-        </div>) :
-        (<>{children}</>);
+    return Loader ? (
+        <div className="bg-[#1e1e1e] flex justify-center items-center h-screen">
+            <style>
+                {`
+        .loader {
+          width: 65px;
+          aspect-ratio: 1;
+          position: relative;
+        }
+        .loader:before,
+        .loader:after {
+          content: "";
+          position: absolute;
+          border-radius: 50px;
+          box-shadow: 0 0 0 3px inset #fff;
+          animation: l5 2.5s infinite;
+        }
+        .loader:after {
+          animation-delay: -1.25s;
+          border-radius: 0;
+        }
+        @keyframes l5 {
+          0% { inset: 0 35px 35px 0; }
+          12.5% { inset: 0 35px 0 0; }
+          25% { inset: 35px 35px 0 0; }
+          37.5% { inset: 35px 0 0 0; }
+          50% { inset: 35px 0 0 35px; }
+          62.5% { inset: 0 0 0 35px; }
+          75% { inset: 0 0 35px 35px; }
+          87.5% { inset: 0 0 35px 0; }
+          100% { inset: 0 35px 35px 0; }
+        }
+      `}
+            </style>
+            <div className="loader"></div>
+        </div>
+    ) : (
+        <>{children}</>
+    );
 }
 
 export default Protection;
